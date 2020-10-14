fa035986ba19f2577a0f08adf6d2c19c3b433ef0
marekru <marekru@users.noreply.github.com>
2020-10-14 11:11:08 +0200

    fix: typo Reaction -> Redaction

--- include/pdfixsdksamples/CreateRedactionMark.h
+++ include/pdfixsdksamples/CreateRedactionMark.h
@@ -6,7 +6,7 @@
 using namespace PDFixSDK;
 
 // Creates redaction mark and saves it to the new document
-void CreateReactionMark(
+void CreateRedactionMark(
   const std::wstring& open_file,                // source PDF document
   const std::wstring& save_file,                // file path where to save PDF docuemnt
   int page_num,                                 // index of page where to create redaction mark
