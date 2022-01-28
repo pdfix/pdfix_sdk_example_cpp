97a2e834e39fba98470cbc7c2b7fdf2114b778ea
Szabi Szamel <szabi.szamel@pdfix.net>
2022-01-28 14:30:04 +0100

    fixed TagsReadingOrder sample

--- src/TagsReadingOrder.cpp
+++ src/TagsReadingOrder.cpp
@@ -13,45 +13,35 @@ using namespace PDFixSDK;
 
 namespace TagsReadingOrder {
 
-  auto struct_elem_deleter = [](PdsStructElement* elem) { elem->Release(); };
-  typedef std::unique_ptr<PdsStructElement, decltype(struct_elem_deleter)> PdsStructElementP;
-
-  PdsStructElement* GetFirstParagraph(PdsStructElement* struct_elem) {
+  bool MoveParagraphToParent(PdsStructElement* struct_elem) {
     // search kid struct elements
-    for (int i = 0; i < struct_elem->GetNumKids(); i++) {
-      if (struct_elem->GetKidType(i) == kPdsStructKidElement) {
-        PdsObject* kid_obj = struct_elem->GetKidObject(i);
-        PdsStructElement* kid_elem = struct_elem->GetStructTree()->GetStructElement(kid_obj);
+    for (int i = 0; i < struct_elem->GetNumChildren(); i++) {
+      if (struct_elem->GetChildType(i) == kPdsStructChildElement) {
+        PdsObject* kid_obj = struct_elem->GetChildObject(i);
+        PdsStructElement* kid_elem = struct_elem->GetStructTree()->GetStructElementFromObject(kid_obj);
         if (!kid_elem)
           throw PdfixException();
         
         auto type = kid_elem->GetType(true);
-        if (type == L"P")
-          return kid_elem;
-
-        auto paragraph = GetFirstParagraph(kid_elem.get());
-        if (paragraph)
-          return paragraph;
+        if (type == L"P") 
+        {
+          if (!struct_elem->MoveChild(i, struct_elem, struct_elem->GetNumChildren() - 1))
+            throw PdfixException();
+          return true;
+        }
+        return MoveParagraphToParent(kid_elem);
       }
     }
-    return nullptr;
-    //return PdsStructElementP(nullptr, struct_elem_deleter);
+    return false;
   }
 
-  PdsStructElement* GetFirstParagraph(PdsStructTree* struct_tree) {
-    for (int i = 0; i < struct_tree->GetNumKids(); i++) {
-      PdsObject* kid_obj = struct_tree->GetKidObject(i);
-      auto elem_deleter = [](PdsStructElement* elem) { elem->Release(); };
-      //std::unique_ptr<PdsStructElement, decltype(elem_deleter)>
-//        kid_elem(struct_tree->AcquireStructElement(kid_obj), elem_deleter);
-
-    PdsStructElement* kid_elem = struct_tree->GetStructElement(kid_obj),
-      auto paragraph = GetFirstParagraph(kid_elem.get());
-      if (paragraph)
-        return paragraph;
+  bool MoveParagraphToParent(PdsStructTree* struct_tree) {
+    for (int i = 0; i < struct_tree->GetNumChildren(); i++) {
+      PdsObject* kid_obj = struct_tree->GetChildObject(i);
+      PdsStructElement* kid_elem = struct_tree->GetStructElementFromObject(kid_obj);
+      return MoveParagraphToParent(kid_elem);
     }
-    return nullptr;
-    //return PdsStructElementP(nullptr, struct_elem_deleter);
+    return false;
   }
 
   void Run(
@@ -77,21 +67,9 @@ namespace TagsReadingOrder {
     if (!struct_tree)
       throw PdfixException();
 
-    // get the first P struct element on the first page
-    PdsStructElementP paragraph = GetFirstParagraph(struct_tree);
-    if (!paragraph)
-      throw std::runtime_error("No paragraph found.");
-
     // move paragraph to the back of it's parent
-    //PdsStructElementP parent(struct_tree->AcquireStructElement(paragraph->GetParentObject()),
-    //  struct_elem_deleter);
-    PdsStructElement* parent = struct_tree->GetStructElement(paragraph->GetParentObject()),
-      struct_elem_deleter);
-    if (!parent)
-      throw PdfixException();
-    
-    if (!paragraph->SetParent(parent.get(), parent->GetNumKids()-1))
-      throw PdfixException();
+    if (!MoveParagraphToParent(struct_tree))
+      throw std::runtime_error("No paragraph found.");
 
     // save document
     if (!doc->Save(save_path.c_str(), kSaveFull))
