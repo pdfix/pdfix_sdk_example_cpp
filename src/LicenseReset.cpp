08b2217026d22bcae6f69cffe121e81cc492b553
jbaranec <jozef.baranec@gmail.com>
2022-09-26 22:34:37 +0200

    license manager API update

--- src/LicenseReset.cpp
+++ src/LicenseReset.cpp
@@ -6,30 +6,30 @@
 #include "pdfixsdksamples/LicenseReset.h"
 
 // system
-#include <string>
 #include <iostream>
+#include <string>
 // other libraries
 #include "Pdfix.h"
 
 using namespace PDFixSDK;
 namespace LicenseReset {
-    // write license status into an output stream
-  void Run() {
-    // initialize Pdfix
-    if (!Pdfix_init(Pdfix_MODULE_NAME))
-      throw std::runtime_error("Pdfix initialization fail");
+// write license status into an output stream
+void Run() {
+  // initialize Pdfix
+  if (!Pdfix_init(Pdfix_MODULE_NAME))
+    throw std::runtime_error("Pdfix initialization fail");
 
-    Pdfix* pdfix = GetPdfix();
-    if (!pdfix)
-      throw std::runtime_error("GetPdfix fail");
+  Pdfix* pdfix = GetPdfix();
+  if (!pdfix)
+    throw std::runtime_error("GetPdfix fail");
 
-    auto authorization = pdfix->GetStandardAuthorization();
-    if (!authorization)
-      throw PdfixException();
-      
-    if (!authorization->Reset())
-      throw PdfixException();
+  auto authorization = pdfix->GetStandardAuthorization();
+  if (!authorization)
+    throw PdfixException();
 
-    pdfix->Destroy();
-  }
+  if (!authorization->Reset())
+    throw PdfixException();
+
+  pdfix->Destroy();
 }
+}  // namespace LicenseReset
--- src/StandardLicenseUpdate.cpp
+++ src/StandardLicenseUpdate.cpp
@@ -5,30 +5,30 @@
 
 #include "pdfixsdksamples/StandardLicenseUpdate.h"
 // system
-#include <string>
 #include <iostream>
+#include <string>
 // other libraries
 #include "Pdfix.h"
 
 using namespace PDFixSDK;
 namespace StandardLicenseUpdate {
-    // Adds a new text annotation.
-  void Run() {
-    // initialize Pdfix
-    if (!Pdfix_init(Pdfix_MODULE_NAME))
-      throw std::runtime_error("Pdfix initialization fail");
+// Adds a new text annotation.
+void Run() {
+  // initialize Pdfix
+  if (!Pdfix_init(Pdfix_MODULE_NAME))
+    throw std::runtime_error("Pdfix initialization fail");
 
-    Pdfix* pdfix = GetPdfix();
-    if (!pdfix)
-      throw std::runtime_error("GetPdfix fail");
+  Pdfix* pdfix = GetPdfix();
+  if (!pdfix)
+    throw std::runtime_error("GetPdfix fail");
 
-    auto authorization = pdfix->GetStandardAuthorization();
-    if (!authorization)
-      throw PdfixException();
-      
-    if (!authorization->Update())
-      throw PdfixException();
-    
-    pdfix->Destroy();
-  }
+  auto authorization = pdfix->GetStandardAuthorization();
+  if (!authorization)
+    throw PdfixException();
+
+  if (!authorization->Update(false))
+    throw PdfixException();
+
+  pdfix->Destroy();
 }
+}  // namespace StandardLicenseUpdate
