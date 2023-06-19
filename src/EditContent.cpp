9ea6b906314f3530f214f3ff513dc4e729d67ca0
jbaranec <jozef.baranec@gmail.com>
2023-06-19 19:51:43 +0200

    Removed method PdsPageObject::SetMatrix, using Transform instead

--- src/EditContent.cpp
+++ src/EditContent.cpp
@@ -8,52 +8,54 @@
 #include "Pdfix.h"
 #include "pdfixsdksamples/PdfixEngine.h"
 
-#include <sstream>
 #include <iterator>
+#include <sstream>
 
 using namespace PDFixSDK;
 
 namespace EditContent {
 
-  PropsBuilder& PropsBuilder::AddText(float x, float y, const std::wstring& text) {
-    ObjectProps props;
-    props.pos.x = x;
-    props.pos.y = y;
-    props.obj_type = PdfPageObjectType::kPdsPageText;
-    props.data = text;
-    m_properties.push_back(props);return *this;
-  }
+PropsBuilder& PropsBuilder::AddText(float x, float y, const std::wstring& text) {
+  ObjectProps props;
+  props.pos.x = x;
+  props.pos.y = y;
+  props.obj_type = PdfPageObjectType::kPdsPageText;
+  props.data = text;
+  m_properties.push_back(props);
+  return *this;
+}
 
-  PropsBuilder& PropsBuilder::AddPath(float x, float y, const std::wstring& svg) {
-    ObjectProps props;
-    props.pos.x = x;
-    props.pos.y = y;
-    props.obj_type = PdfPageObjectType::kPdsPagePath;
-    props.data = svg;
-    m_properties.push_back(props);
-    return *this;
-  }
+PropsBuilder& PropsBuilder::AddPath(float x, float y, const std::wstring& svg) {
+  ObjectProps props;
+  props.pos.x = x;
+  props.pos.y = y;
+  props.obj_type = PdfPageObjectType::kPdsPagePath;
+  props.data = svg;
+  m_properties.push_back(props);
+  return *this;
+}
 
-  PropsBuilder& PropsBuilder::AddImage(float x, float y, const std::wstring& path) {
-    ObjectProps props;
-    props.pos.x = x;
-    props.pos.y = y;
-    props.obj_type = PdfPageObjectType::kPdsPageImage;
-    props.data = path;
-    m_properties.push_back(props);
-    return *this;
-  }
+PropsBuilder& PropsBuilder::AddImage(float x, float y, const std::wstring& path) {
+  ObjectProps props;
+  props.pos.x = x;
+  props.pos.y = y;
+  props.obj_type = PdfPageObjectType::kPdsPageImage;
+  props.data = path;
+  m_properties.push_back(props);
+  return *this;
+}
 
-  std::vector<ObjectProps>& PropsBuilder::Get() {
-    return m_properties;
-  }
+std::vector<ObjectProps>& PropsBuilder::Get() {
+  return m_properties;
+}
 
-  // edit content
-  void EditPageContent(Pdfix* pdfix, PdfDoc* doc, PdsContent* content, const std::vector<ObjectProps>& object_props) {
-
-    for (auto& object_prop : object_props) {
-      switch (object_prop.obj_type)
-      {
+// edit content
+void EditPageContent(Pdfix* pdfix,
+                     PdfDoc* doc,
+                     PdsContent* content,
+                     const std::vector<ObjectProps>& object_props) {
+  for (auto& object_prop : object_props) {
+    switch (object_prop.obj_type) {
       case PdfPageObjectType::kPdsPageText:
         AddText(pdfix, doc, content, object_prop);
         break;
@@ -65,218 +67,214 @@ namespace EditContent {
         break;
       default:
         throw std::runtime_error("not implemented");
-      }
     }
   }
+}
 
-  void AddText(Pdfix* pdfix, PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
+void AddText(Pdfix* pdfix, PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
+  auto rgb_colorspace = doc->CreateColorSpace(PdfColorSpaceFamily::kColorSpaceDeviceRGB);
 
-    auto rgb_colorspace = doc->CreateColorSpace(PdfColorSpaceFamily::kColorSpaceDeviceRGB);
+  auto matrix = PdfMatrix();
+  matrix.a = 1;
+  matrix.b = 0;
+  matrix.c = 0;
+  matrix.d = 1;
+  matrix.e = object_prop.pos.x;
+  matrix.f = object_prop.pos.y;
 
-    auto matrix = PdfMatrix();
-    matrix.a = 1;
-    matrix.b = 0;
-    matrix.c = 0;
-    matrix.d = 1;
-    matrix.e = object_prop.pos.x;
-    matrix.f = object_prop.pos.y;
+  auto sys_font =
+      pdfix->FindSysFont(L"Arial", kFontForceBold, PdfFontCodepage::kFontDefANSICodepage);
 
-    auto sys_font = pdfix->FindSysFont(L"Arial", kFontForceBold, PdfFontCodepage::kFontDefANSICodepage);
+  if (!sys_font) {
+    throw PdfixException();
+  }
 
-    if (!sys_font) {
-      throw PdfixException();
+  auto font = doc->CreateFont(sys_font, PdfFontCharset::kFontAnsiCharset, 0);
+  sys_font->Destroy();
+
+  auto text_obj = content->AddNewText(-1, font, &matrix);
+  if (!text_obj)
+    throw PdfixException();
+
+  text_obj->SetText(object_prop.data.c_str());
+
+  PdfTextState ts;
+  auto stroke_color = rgb_colorspace->CreateColor();
+  stroke_color->SetValue(0, 1.0f);
+  ts.color_state.stroke_color = stroke_color;
+  ts.color_state.stroke_opacity = 255;
+  ts.color_state.stroke_type = kFillTypeSolid;
+
+  auto fill_color = rgb_colorspace->CreateColor();
+  fill_color->SetValue(1, 1.0f);
+  ts.color_state.fill_color = fill_color;
+  ts.color_state.fill_opacity = 255;
+  ts.color_state.fill_type = kFillTypeSolid;
+
+  ts.font = font;
+  ts.font_size = 20;
+
+  ts.char_spacing = 7;
+  ts.word_spacing = 10;
+
+  text_obj->SetTextState(&ts);
+}
+
+void CreatePathFromSvg(std::wstring svg_path, PdsPath* path_obj) {
+  std::wistringstream iss(svg_path);
+  std::istream_iterator<std::wstring, wchar_t, std::char_traits<wchar_t>> it(iss);
+  auto end = std::istream_iterator<std::wstring, wchar_t, std::char_traits<wchar_t>>();
+  for (; it != end; ++it) {
+    auto token = *it;
+    if (token == L"M") {
+      PdfPoint pt;
+      pt.x = std::stod(*(++it));
+      pt.y = std::stod(*(++it));
+      path_obj->MoveTo(&pt);
+    } else if (token == L"L") {
+      PdfPoint pt;
+      pt.x = std::stod(*(++it));
+      pt.y = std::stod(*(++it));
+      path_obj->LineTo(&pt);
+    } else if (token == L"C") {
+      PdfPoint cp1;
+      cp1.x = std::stod(*(++it));
+      cp1.y = std::stod(*(++it));
+
+      PdfPoint cp2;
+      cp2.x = std::stod(*(++it));
+      cp2.y = std::stod(*(++it));
+
+      PdfPoint pt;
+      pt.x = std::stod(*(++it));
+      pt.y = std::stod(*(++it));
+      path_obj->CurveTo(&cp1, &cp2, &pt);
+    } else if (token == L"A") {
+      PdfPoint r;
+      r.x = std::stod(*(++it));
+      r.y = std::stod(*(++it));
+
+      float x_angle = std::stod(*(++it));
+
+      bool large = std::stoi(*(++it)) != 0;
+      bool sweep = std::stoi(*(++it)) != 0;
+
+      PdfPoint to;
+      to.x = std::stod(*(++it));
+      to.y = std::stod(*(++it));
+
+      path_obj->ArcTo(&to, &r, x_angle, large, sweep);
+    } else if (token == L"Z") {
+      path_obj->ClosePath();
     }
-
-    auto font = doc->CreateFont(sys_font, PdfFontCharset::kFontAnsiCharset, 0);
-    sys_font->Destroy();
-
-    auto text_obj = content->AddNewText(-1, font, &matrix);
-    if (!text_obj)
-      throw PdfixException();
-
-    text_obj->SetText(object_prop.data.c_str());
-
-    PdfTextState ts;
-    auto stroke_color = rgb_colorspace->CreateColor();
-    stroke_color->SetValue(0, 1.0f);
-    ts.color_state.stroke_color = stroke_color;
-    ts.color_state.stroke_opacity = 255;
-    ts.color_state.stroke_type = kFillTypeSolid;
-
-    auto fill_color = rgb_colorspace->CreateColor();
-    fill_color->SetValue(1, 1.0f);
-    ts.color_state.fill_color = fill_color;
-    ts.color_state.fill_opacity = 255;
-    ts.color_state.fill_type = kFillTypeSolid;
-
-    ts.font = font;
-    ts.font_size = 20;
-
-    ts.char_spacing = 7;
-    ts.word_spacing = 10;
-
-    text_obj->SetTextState(&ts);
   }
+}
 
-  void CreatePathFromSvg(std::wstring svg_path, PdsPath* path_obj) {
-    std::wistringstream iss(svg_path);
-    std::istream_iterator<std::wstring, wchar_t, std::char_traits<wchar_t>> it(iss);
-    auto end = std::istream_iterator<std::wstring, wchar_t, std::char_traits<wchar_t>>();
-    for (;it != end; ++it) {
-      auto token = *it;
-      if (token == L"M") {
-        PdfPoint pt;
-        pt.x = std::stod(*(++it));
-        pt.y = std::stod(*(++it));
-        path_obj->MoveTo(&pt);
-      }else if (token == L"L") {
-        PdfPoint pt;
-        pt.x = std::stod(*(++it));
-        pt.y = std::stod(*(++it));
-        path_obj->LineTo(&pt);
-      } else if (token == L"C") {
-        PdfPoint cp1;
-        cp1.x = std::stod(*(++it));
-        cp1.y = std::stod(*(++it));
+// path
+void AddPath(PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
+  auto rgb_colorspace = doc->CreateColorSpace(PdfColorSpaceFamily::kColorSpaceDeviceRGB);
 
-        PdfPoint cp2;
-        cp2.x = std::stod(*(++it));
-        cp2.y = std::stod(*(++it));
+  PdfMatrix matrix;
+  matrix.a = 1;
+  matrix.b = 0;
+  matrix.c = 0;
+  matrix.d = 1;
+  matrix.e = object_prop.pos.x;
+  matrix.f = object_prop.pos.y;
 
-        PdfPoint pt;
-        pt.x = std::stod(*(++it));
-        pt.y = std::stod(*(++it));
-        path_obj->CurveTo(&cp1, &cp2, &pt);
-      } else if (token == L"A") {
-        PdfPoint r;
-        r.x = std::stod(*(++it));
-        r.y = std::stod(*(++it));
+  auto path_obj = content->AddNewPath(-1, &matrix);
+  if (!path_obj)
+    throw PdfixException();
 
-        float x_angle = std::stod(*(++it));
+  CreatePathFromSvg(object_prop.data, path_obj);
 
-        bool large = std::stoi(*(++it)) != 0;
-        bool sweep = std::stoi(*(++it)) != 0;
+  path_obj->SetStroke(true);
+  path_obj->SetFillType(kFillRuleEvenOdd);
 
-        PdfPoint to;
-        to.x = std::stod(*(++it));
-        to.y = std::stod(*(++it));
+  PdfGraphicState gs;
+  // stroke blue color
+  auto stroke_color = rgb_colorspace->CreateColor();
+  stroke_color->SetValue(0, 0.2f);
+  stroke_color->SetValue(1, 0.7f);
+  stroke_color->SetValue(2, 0.8f);
 
-        path_obj->ArcTo(&to, &r, x_angle, large, sweep);
-      } else if (token == L"Z") {
-        path_obj->ClosePath();
-      }
-    }
-  }
+  gs.color_state.stroke_color = stroke_color;
+  gs.color_state.stroke_opacity = 255;
+  gs.color_state.stroke_type = kFillTypeSolid;
 
-  //path
-  void AddPath(PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
+  // fill red color
+  auto fill_color = rgb_colorspace->CreateColor();
+  fill_color->SetValue(0, 0.99f);
+  fill_color->SetValue(1, 0.33f);
+  fill_color->SetValue(2, 0.33f);
+  //  gs.color_state.fill_color = fill_color;
+  //  gs.color_state.fill_opacity = 255;
+  //  gs.color_state.fill_type = kFillTypeSolid;
 
-    auto rgb_colorspace = doc->CreateColorSpace(PdfColorSpaceFamily::kColorSpaceDeviceRGB);
+  //  gs.line_width = 3.0;
+  //  gs.miter_limit = 1.0;
+  //  gs.line_cap = kPdfLineCapRound;
+  //  gs.line_join = kPdfLineJoinRound;
+  //  gs.blend_mode = kBlendModeNormal;
 
-    PdfMatrix matrix;
-    matrix.a = 1;
-    matrix.b = 0;
-    matrix.c = 0;
-    matrix.d = 1;
-    matrix.e = object_prop.pos.x;
-    matrix.f = object_prop.pos.y;
+  path_obj->TransformCTM(&matrix);
+  //  gs.matrix = matrix;
 
-    auto path_obj = content->AddNewPath(-1, &matrix);
-    if (!path_obj)
-      throw PdfixException();
+  //  path_obj->SetGState(&gs);
+}
 
-    CreatePathFromSvg(object_prop.data, path_obj);
+// image
+void AddImage(Pdfix* pdfix, PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
+  auto& img_path = object_prop.data;
+  auto image_stm = pdfix->CreateFileStream(img_path.c_str(), kPsReadOnly);
+  if (!image_stm)
+    throw PdfixException();
+  PdfImageFormat format = kImageFormatJpg;
+  if ((img_path.rfind(L".png") != std::wstring::npos) ||
+      (img_path.rfind(L".PNG") != std::wstring::npos))
+    format = kImageFormatPng;
+  auto xobj = doc->CreateXObjectFromImage(image_stm, format, 0);
+  auto image_dict = xobj->GetStreamDict();
+  auto width = image_dict->GetNumber(L"Width");
+  auto height = image_dict->GetNumber(L"Height");
+  auto matrix = PdfMatrix();
+  matrix.a = width;
+  matrix.b = 0;
+  matrix.c = 0;
+  matrix.d = height;
+  matrix.e = object_prop.pos.x;
+  matrix.f = object_prop.pos.y;
+  auto image_obj = content->AddNewImage(-1, xobj, &matrix);
+}
 
-    path_obj->SetStroke(true);
-    path_obj->SetFillType(kFillRuleEvenOdd);
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void Run(const std::wstring& output_path,              // output PDF document
+         const std::vector<ObjectProps>& object_props  // structure containing object properties
+) {
+  auto pdfix = PdfixEngine::Get();
 
-    PdfGraphicState gs;
-    // stroke blue color
-    auto stroke_color = rgb_colorspace->CreateColor();
-    stroke_color->SetValue(0, 0.2f);
-    stroke_color->SetValue(1, 0.7f);
-    stroke_color->SetValue(2, 0.8f);
+  auto doc = pdfix->CreateDoc();
+  if (!doc)
+    throw PdfixException();
 
-    gs.color_state.stroke_color = stroke_color;
-    gs.color_state.stroke_opacity = 255;
-    gs.color_state.stroke_type = kFillTypeSolid;
+  PdfRect mediabox;
+  mediabox.left = 0;
+  mediabox.top = 792;
+  mediabox.right = 621;
+  mediabox.bottom = 0;
+  auto page = doc->CreatePage(-1, &mediabox);
+  if (!page)
+    throw PdfixException();
+  auto content = page->GetContent();
 
-    // fill red color
-    auto fill_color = rgb_colorspace->CreateColor();
-    fill_color->SetValue(0, 0.99f);
-    fill_color->SetValue(1, 0.33f);
-    fill_color->SetValue(2, 0.33f);
-    gs.color_state.fill_color = fill_color;
-    gs.color_state.fill_opacity = 255;
-    gs.color_state.fill_type = kFillTypeSolid;
-    
-    gs.line_width = 3.0;
-    gs.miter_limit = 1.0;
-    gs.line_cap = kPdfLineCapRound;
-    gs.line_join = kPdfLineJoinRound;
-    gs.blend_mode = kBlendModeNormal;
+  EditPageContent(pdfix, doc, content, object_props);
 
-    gs.matrix = matrix;
+  page->SetContent();
 
-    path_obj->SetGState(&gs);
-  }
+  if (!doc->Save(output_path.c_str(), kSaveFull))
+    throw PdfixException();
 
-  //image
-  void AddImage(Pdfix* pdfix, PdfDoc* doc, PdsContent* content, const ObjectProps& object_prop) {
-    auto& img_path = object_prop.data;
-    auto image_stm = pdfix->CreateFileStream(img_path.c_str(), kPsReadOnly);
-    if (!image_stm)
-      throw PdfixException();
-    PdfImageFormat format = kImageFormatJpg;
-    if ((img_path.rfind(L".png") != std::wstring::npos)
-      || (img_path.rfind(L".PNG") != std::wstring::npos))
-      format = kImageFormatPng;
-    auto xobj = doc->CreateXObjectFromImage(image_stm, format, 0);
-    auto image_dict = xobj->GetStreamDict();
-    auto width = image_dict->GetNumber(L"Width");
-    auto height = image_dict->GetNumber(L"Height");
-    auto matrix = PdfMatrix();
-    matrix.a = width;
-    matrix.b = 0;
-    matrix.c = 0;
-    matrix.d = height;
-    matrix.e = object_prop.pos.x;
-    matrix.f = object_prop.pos.y;
-    auto image_obj = content->AddNewImage(-1, xobj, &matrix);
-
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////
-  void Run(
-    const std::wstring& output_path,               // output PDF document
-    const std::vector<ObjectProps>& object_props   // structure containing object properties
-    )
-  {
-    auto pdfix = PdfixEngine::Get();
-
-    auto doc = pdfix->CreateDoc();
-    if (!doc)
-      throw PdfixException();
-    
-    PdfRect mediabox;
-    mediabox.left = 0;
-    mediabox.top  = 792;
-    mediabox.right = 621;
-    mediabox.bottom = 0;
-    auto page = doc->CreatePage(-1, &mediabox);
-    if (!page)
-      throw PdfixException();
-    auto content = page->GetContent();
-    
-    EditPageContent(pdfix, doc, content, object_props);
-        
-    page->SetContent();
-
-    if (!doc->Save(output_path.c_str(), kSaveFull))
-      throw PdfixException();
-
-    page->Release();
-    doc->Close();
-    
-  }
-} // namespace EditContent
+  page->Release();
+  doc->Close();
+}
+}  // namespace EditContent
--- src/ExtractPdfUtils.cpp
+++ src/ExtractPdfUtils.cpp
@@ -11,154 +11,152 @@
 #include "Pdfix.h"
 
 extern std::string ToUtf8(const std::wstring& wstr);
-extern std::string PsStreamEncodeBase64(PsStream *stream);
+extern std::string PsStreamEncodeBase64(PsStream* stream);
 
 namespace ExtractData {
-  auto put_array_node = [](auto &node, auto &value) {
-    ptree value_node;
-    value_node.put("", value);
-    node.push_back(std::make_pair("", value_node));
+auto put_array_node = [](auto& node, auto& value) {
+  ptree value_node;
+  value_node.put("", value);
+  node.push_back(std::make_pair("", value_node));
+};
+
+void ExtractBBox(PdfRect bbox, ptree& node, const DataType& data_types) {
+  ptree left_node, bottom_node, right_node, top_node;
+  left_node.put("", bbox.left);
+  node.push_back(std::make_pair("", left_node));
+  bottom_node.put("", bbox.bottom);
+  node.push_back(std::make_pair("", bottom_node));
+  right_node.put("", bbox.right);
+  node.push_back(std::make_pair("", right_node));
+  top_node.put("", bbox.top);
+  node.push_back(std::make_pair("", top_node));
+}
+
+void ExtractMatrix(const PdfMatrix& matrix, ptree& node, const DataType& data_types) {
+  put_array_node(node, matrix.a);
+  put_array_node(node, matrix.b);
+  put_array_node(node, matrix.c);
+  put_array_node(node, matrix.d);
+  put_array_node(node, matrix.e);
+  put_array_node(node, matrix.f);
+}
+
+void ExtractColorState(const PdfColorState& color_state, ptree& node, const DataType& data_types) {
+  ptree color_state_node;
+
+  if (color_state.fill_color) {
+    node.put("fill_color_space", "rgb");
+    node.put("fill_color_opacity", color_state.fill_opacity);
+    ptree fill_color_node;
+    auto fill_color = color_state.fill_color->GetRGB();
+    put_array_node(fill_color_node, fill_color.r);
+    put_array_node(fill_color_node, fill_color.g);
+    put_array_node(fill_color_node, fill_color.b);
+    node.put_child("fill_color", fill_color_node);
+  }
+
+  if (color_state.stroke_color) {
+    node.put("stroke_color_space", "rgb");
+    node.put("stroke_color_opacity", color_state.stroke_opacity);
+    ptree stroke_color_node;
+    auto stroke_color = color_state.stroke_color->GetRGB();
+    put_array_node(stroke_color_node, stroke_color.r);
+    put_array_node(stroke_color_node, stroke_color.g);
+    put_array_node(stroke_color_node, stroke_color.b);
+    node.put_child("stroke_color", stroke_color_node);
+  }
+}
+
+void ExtractTextState(PdfTextState* text_state, ptree& node, const DataType& data_types) {
+  ptree color_state_node;
+  ExtractColorState(text_state->color_state, color_state_node, data_types);
+  node.put_child("color_state", color_state_node);
+
+  if (text_state->font) {
+    node.put("font_name", EncodeText(text_state->font->GetFontName()));
+  }
+  node.put("font_size", text_state->font_size);
+}
+
+void ExtractGraphicState(const PdfGraphicState& graphic_state,
+                         ptree& node,
+                         const DataType& data_types) {
+  //    ptree matrix_node;
+  //    ExtractMatrix(graphic_state.matrix, matrix_node, data_types);
+  //    node.put_child("matrix", matrix_node);
+
+  ptree color_state_node;
+  ExtractColorState(graphic_state.color_state, color_state_node, data_types);
+  node.put_child("color_state", color_state_node);
+
+  node.put("line_width", graphic_state.line_width);
+}
+
+// render page are into an image
+void RenderPageArea(PdfPage* page, PdfRect& bbox, ptree& node, const DataType& data_types) {
+  auto page_view_deleter = [&](PdfPageView* page_view) { page_view->Release(); };
+  auto page_view = std::unique_ptr<PdfPageView, decltype(page_view_deleter)>(
+      page->AcquirePageView(data_types.render_zoom, data_types.render_rotate), page_view_deleter);
+  if (!page_view)
+    throw PdfixException();
+
+  // calculate the image bounding box
+  PdfDevRect elem_dev_rect;
+  page_view->RectToDevice(&bbox, &elem_dev_rect);
+  int elem_width = elem_dev_rect.right - elem_dev_rect.left;
+  int elem_height = elem_dev_rect.bottom - elem_dev_rect.top;
+  if (elem_height == 0 || elem_width == 0)
+    return;
+
+  // prepare the image
+  PsImage* ps_image = GetPdfix()->CreateImage(page_view->GetDeviceWidth(),
+                                              page_view->GetDeviceHeight(), kImageDIBFormatArgb);
+  if (!ps_image)
+    throw PdfixException();
+
+  PdfPageRenderParams render_params;
+  render_params.image = ps_image;
+  page_view->GetDeviceMatrix(&render_params.matrix);
+  page->DrawContent(&render_params, nullptr, nullptr);
+
+  PdfImageParams img_params;
+  img_params.format = data_types.image_format;
+
+  // save image to memory stream, use CreateFileStream to save image to file
+  auto stm = GetPdfix()->CreateMemStream();
+  if (!stm)
+    throw PdfixException();
+  ps_image->SaveRectToStream(stm, &img_params, &elem_dev_rect);
+  ps_image->Destroy();
+
+  // save image to ptree as base64 stream
+  node.put("base64", PsStreamEncodeBase64(stm));
+
+  stm->Destroy();
+}
+
+std::string EncodeText(const std::wstring& text) {
+  // https://www.w3schools.com/html/html_charset.asp
+  std::wstring replace[] = {L"<",  L"&#60;",  L">", L"&#62;",  L"&", L"&#38;",  L"\"", L"&#34;",
+                            L"\\", L"&#39;",  L"�", L"&#162;", L"�", L"&#163;", L"�",  L"&#165;",
+                            L"�",  L"&#128;", L"�", L"&#169;", L"�", L"&#174;", L"",   L""};
+
+  std::wstring result = text;
+  auto replace_all = [&](const std::wstring& from, const std::wstring& to) {
+    size_t pos = 0;
+    while ((pos = result.find(from, pos)) != std::string::npos) {
+      result.replace(pos, from.length(), to);
+      pos += to.length();
+    }
   };
 
-  void ExtractBBox(PdfRect bbox, ptree& node, const DataType& data_types) {
-    ptree left_node, bottom_node, right_node, top_node;
-    left_node.put("", bbox.left);
-    node.push_back(std::make_pair("", left_node));
-    bottom_node.put("", bbox.bottom);
-    node.push_back(std::make_pair("", bottom_node));
-    right_node.put("", bbox.right);
-    node.push_back(std::make_pair("", right_node));
-    top_node.put("", bbox.top);
-    node.push_back(std::make_pair("", top_node));
+  int i = 0;
+  while (1) {
+    if (replace[i].empty())
+      break;
+    replace_all(replace[i], replace[i]);
+    i += 2;
   }
-
-  void ExtractMatrix(const PdfMatrix &matrix, ptree& node, const DataType& data_types) {
-    put_array_node(node, matrix.a);
-    put_array_node(node, matrix.b);
-    put_array_node(node, matrix.c);
-    put_array_node(node, matrix.d);
-    put_array_node(node, matrix.e);
-    put_array_node(node, matrix.f);
-  }  
-
-  void ExtractColorState(const PdfColorState &color_state, ptree &node, const DataType &data_types) {
-    ptree color_state_node;
-    
-    if (color_state.fill_color) {
-      node.put("fill_color_space", "rgb");
-      node.put("fill_color_opacity", color_state.fill_opacity);
-      ptree fill_color_node;
-      auto fill_color = color_state.fill_color->GetRGB();
-      put_array_node(fill_color_node, fill_color.r);
-      put_array_node(fill_color_node, fill_color.g);
-      put_array_node(fill_color_node, fill_color.b);
-      node.put_child("fill_color", fill_color_node);
-    }
-
-    if (color_state.stroke_color) {
-      node.put("stroke_color_space", "rgb");
-      node.put("stroke_color_opacity", color_state.stroke_opacity);
-      ptree stroke_color_node;
-      auto stroke_color = color_state.stroke_color->GetRGB();
-      put_array_node(stroke_color_node, stroke_color.r);
-      put_array_node(stroke_color_node, stroke_color.g);
-      put_array_node(stroke_color_node, stroke_color.b);
-      node.put_child("stroke_color", stroke_color_node);
-    }
-  }
-
-  void ExtractTextState(PdfTextState *text_state, ptree &node, const DataType &data_types) {
-    ptree color_state_node;
-    ExtractColorState(text_state->color_state, color_state_node, data_types);
-    node.put_child("color_state", color_state_node);
-
-    if (text_state->font) {
-      node.put("font_name", EncodeText(text_state->font->GetFontName()));
-    }
-    node.put("font_size", text_state->font_size);
-  }
-
-  void ExtractGraphicState(const PdfGraphicState &graphic_state, ptree &node, const DataType &data_types) {  
-    ptree matrix_node;
-    ExtractMatrix(graphic_state.matrix, matrix_node, data_types);
-    node.put_child("matrix", matrix_node);
-
-    ptree color_state_node;
-    ExtractColorState(graphic_state.color_state, color_state_node, data_types);
-    node.put_child("color_state", color_state_node);
-
-    node.put("line_width", graphic_state.line_width);
-  }
-
-  // render page are into an image
-  void RenderPageArea(PdfPage* page, PdfRect& bbox, ptree& node, const DataType &data_types) {
-
-    auto page_view_deleter = [&](PdfPageView *page_view) { page_view->Release(); };
-    auto page_view = std::unique_ptr<PdfPageView, 
-          decltype(page_view_deleter)>(page->AcquirePageView(
-            data_types.render_zoom, data_types.render_rotate), 
-          page_view_deleter);
-    if (!page_view)
-      throw PdfixException();
-
-    // calculate the image bounding box
-    PdfDevRect elem_dev_rect;
-    page_view->RectToDevice(&bbox, &elem_dev_rect);
-    int elem_width = elem_dev_rect.right - elem_dev_rect.left;
-    int elem_height = elem_dev_rect.bottom - elem_dev_rect.top;
-    if (elem_height == 0 || elem_width == 0)
-      return;
-    
-    // prepare the image 
-    PsImage* ps_image = GetPdfix()->CreateImage(page_view->GetDeviceWidth(),
-      page_view->GetDeviceHeight(), kImageDIBFormatArgb);
-    if (!ps_image)
-      throw PdfixException();
-
-    PdfPageRenderParams render_params;
-    render_params.image = ps_image;
-    page_view->GetDeviceMatrix(&render_params.matrix);
-    page->DrawContent(&render_params, nullptr, nullptr);
-
-    PdfImageParams img_params;
-    img_params.format = data_types.image_format;
-
-    // save image to memory stream, use CreateFileStream to save image to file
-    auto stm = GetPdfix()->CreateMemStream();
-    if (!stm)
-      throw PdfixException();
-    ps_image->SaveRectToStream(stm, &img_params, &elem_dev_rect);
-    ps_image->Destroy();
-
-    // save image to ptree as base64 stream
-    node.put("base64", PsStreamEncodeBase64(stm));
-
-    stm->Destroy();
-  }  
-
-  std::string EncodeText(const std::wstring& text) {
-    // https://www.w3schools.com/html/html_charset.asp
-    std::wstring replace[] = {
-      L"<", L"&#60;",   L">", L"&#62;",   L"&", L"&#38;",   L"\"", L"&#34;",  L"\\", L"&#39;",
-      L"�", L"&#162;",  L"�", L"&#163;",  L"�", L"&#165;",  L"�", L"&#128;",  L"�", L"&#169;",
-      L"�", L"&#174;",  L"", L""
-    };
-
-    std::wstring result = text;
-    auto replace_all = [&](const std::wstring &from, const std::wstring &to) {
-      size_t pos = 0;
-      while ((pos = result.find(from, pos)) != std::string::npos) {
-        result.replace(pos, from.length(), to);
-        pos += to.length();
-      }
-    };
-
-    int i = 0;
-    while (1) {
-      if (replace[i].empty()) break;
-      replace_all(replace[i], replace[i]);
-      i += 2;
-    }
-    return ToUtf8(result);
-  }  
+  return ToUtf8(result);
 }
+}  // namespace ExtractData
