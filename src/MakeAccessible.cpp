88d7dd898fdfa06537244593bda622611a886767
Jozef Baranec <jozef.baranec@gmail.com>
2026-03-06 13:33:10 +0100

    Update API for v8.7.17

--- src/MakeAccessible.cpp
+++ src/MakeAccessible.cpp
@@ -5,6 +5,7 @@
 
 #include "pdfixsdksamples/MakeAccessible.h"
 
+#include <boost/json.hpp>
 #include <iostream>
 #include <memory>
 #include <optional>
@@ -35,37 +36,70 @@ void MakeAccessible(
   PsStream* cmd_stm = nullptr;
 
   if (command_path.empty()) {
-    cmd_stm = pdfix->CreateMemStream();
-    if (!cmd_stm || !command->SaveCommandsToStream(kActionMakeAccessible, cmd_stm, kDataFormatJson,
-                                                   kSaveFull)) {
+    // find the make-accessible command in all embedded commands
+    auto cmd_count = command->GetNumCustomActions();
+
+    if (command_path.empty()) {
+      for (int i = 0; i < cmd_count; i++) {
+        cmd_stm = pdfix->CreateMemStream();
+        if (!cmd_stm) {
+          throw PdfixException();
+        }
+
+        if (!command->SaveCustomActionToStream(i, cmd_stm, kDataFormatJson, kSaveFull)) {
+          throw PdfixException();
+        }
+
+        std::vector<uint8_t> json_content;
+        json_content.resize(cmd_stm->GetSize());
+        cmd_stm->Read(0, json_content.data(), json_content.size());
+
+        // find command with name 'make-accessible'
+        boost::json::value jv;
+        try {
+          jv = boost::json::parse(std::string_view(
+              reinterpret_cast<const char*>(json_content.data()), json_content.size()));
+        } catch (std::exception& ex) {
+          std::cout << "[WARNING]: Loading JSON command failed. [" << std::to_string(i) << "] "
+                    << ex.what() << std::endl;
+        }
+
+        auto& js = jv.as_object();
+        if (js.if_contains("name") && (js.at("name") == "make_accessible")) {
+          break;
+        }
+        cmd_stm->Destroy();
+        cmd_stm = nullptr;
+      }
+    } else {
+      cmd_stm = pdfix->CreateFileStream(command_path.c_str(), kPsReadOnly);
+      if (!cmd_stm) {
+        throw PdfixException();
+      }
+    }
+
+    if (cmd_stm != nullptr) {
+      if (!command->LoadParamsFromStream(cmd_stm, kDataFormatJson)) {
+        throw PdfixException();
+      }
+      cmd_stm->Destroy();
+    }
+
+    if (!command->Run()) {
       throw PdfixException();
     }
-  } else {
-    cmd_stm = pdfix->CreateFileStream(command_path.c_str(), kPsReadOnly);
-    if (!cmd_stm) {
+
+    if ((!language.empty()) && (!doc->SetLang(language.c_str()))) {
       throw PdfixException();
     }
+
+    if ((!title.empty()) && (!doc->SetInfo(L"Title", title.c_str()))) {
+      throw PdfixException();
+    }
+
+    if (!doc->Save(save_path.c_str(), kSaveFull))
+      throw PdfixException();
+
+    doc->Close();
   }
-
-  if (!command->LoadParamsFromStream(cmd_stm, kDataFormatJson)) {
-    throw PdfixException();
-  }
-  cmd_stm->Destroy();
-
-  if (!command->Run()) {
-    throw PdfixException();
-  }
-
-  if ((!language.empty()) && (!doc->SetLang(language.c_str()))) {
-    throw PdfixException();
-  }
-
-  if ((!title.empty()) && (!doc->SetInfo(L"Title", title.c_str()))) {
-    throw PdfixException();
-  }
-
-  if (!doc->Save(save_path.c_str(), kSaveFull))
-    throw PdfixException();
-
-  doc->Close();
 }
